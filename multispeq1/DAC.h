1667ac71833f131959345fa29a9a37c17ee3e639
jzeeff <jon@zeeff.com>
2016-04-05 13:58:40 -0400

    test4

--- multispeq1/DAC.h
+++ multispeq1/DAC.h
@@ -1,8 +1,4 @@
 
-//  Routines to set multispeq DAC values.
-//  Do not write to the DACs or ldac lines anywhere else.
-//  Any calls from outside this file assume that pins are numbered 1-x (vs 0-x).
-//  *** Important: assume that the DAC ICs have addresses of 1,2,3 that were set with other software
 //  Jon Zeeff, March, 2016
 // test
 
@@ -14,9 +10,9 @@
 #define LDAC2    16
 #define LDAC3    17
 
-// DAC routines
+// DAC functions
 int DAC_init(void);
 void DAC_change(void);
-void DAC_set(unsigned int pin, unsigned int value);
+void DAC_set(unsigned int LED, unsigned int value);
 int DAC_set_address(int ldac_pin, unsigned oldAddress, unsigned newAddress); 
 
